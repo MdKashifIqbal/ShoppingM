e13a972102a7e012ac6e9f450bcf243b4047b0b1
MdKashifIqbal <mdkashifiqbal2003@gmail.com>
2025-09-12 17:07:49 +0530

    catching-implemented

--- src/useProduct.js
+++ src/useProduct.js
@@ -6,13 +6,16 @@ export function useProduct({ limit, page_no, id }) {
     queryFn: async () => {
       let url = `https://dummyjson.com/products`;
       if (id) {
-        const res = await fetch(`${url}/${id}`)
-        return res.json()
+        const res = await fetch(`${url}/${id}`);
+        return res.json();
       }
       const res = await fetch(
         `${url}?limit=${limit}&skip=${(page_no - 1) * limit}`
       );
       return res.json();
     },
+    keepPreviousData: true,
+    staleTime:1000*60*5,
+    cacheTime:1000*60*10
   });
 }
