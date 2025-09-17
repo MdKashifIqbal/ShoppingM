626d6bdd1cade074be03f9f1af6359f9d16d69a0
MdKashifIqbal <mdkashifiqbal2003@gmail.com>
2025-09-17 16:37:15 +0530

    logout button bug fixed

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -83,8 +83,8 @@ const Cards = () => {
             <option value="mobile-accessories">Mobile-Accessories</option>
           </select>
         </div>
-          <div className="Logout" onClick={handleLogout}>
-            <button>Logout</button>
+          <div className="Logout">
+            <button onClick={handleLogout}>Logout</button>
         </div>
       </div>
       <div className="container">
